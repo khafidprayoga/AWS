5319db7f565a8e00a7d1deda95c607fc99656e1e
Khafid Prayoga <yogaengginerindo@gmail.com>
2021-01-30 15:42:24 +0700

    add setup.py

---
+++ Translate/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='translate',
+    version='0.1',
+    author='Khafid Prayoga',
+    author_email='khafidlinux@gmail.com',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=read('README.md'),
+    install_requires=[
+        'Click',
+        'boto3',
+    ],
+    entry_points='''
+        [console_scripts]
+        translate=translate:translate
+    ''',
+)
